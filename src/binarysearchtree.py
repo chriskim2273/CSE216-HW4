c567380bbc05126bd3ef7023a4fd11702277351d
Chris <87253473+chriskim2273@users.noreply.github.com>
2022-05-04 11:57:01 -0400

    Update binarysearchtree.py

--- src/binarysearchtree.py
+++ src/binarysearchtree.py
@@ -89,7 +89,14 @@ if __name__ == "__main__":
     t2 = BinarySearchTree(name="Birch", root=Node())
     t1.add_all(5, 3, 9, 0)
     t2.add_all(1, 0, 10, 2, 7)
+
+    print()
+    print(t1)
     for x in t1:
         print(x)
+
+    print()
+
+    print(t2)
     for x in t2:
         print(x)
